8d46a843d6dcf26296c0e81a0b732b23fd3fd17a
VladoDev03 <vladsto101@gmail.com>
2021-02-25 23:47:04 +0200

    User service added.

---
+++ FancyLibrary/ConsoleVersion/Services/UserServices.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleVersion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleVersion.Services
+{
+    public static class UserServices
+    {
+        private static FancyLibraryContext db;
+
+        static UserServices()
+        {
+            db = new FancyLibraryContext();
+        }
+
+        public static List<User> GetAllUsers()
+        {
+            return db.Users.ToList();
+        }
+
+        public static void AddUser(User user)
+        {
+            db.Users.Add(user);
+        }
+    }
+}
