1127f86653dafee2e556636837398a8dd616ab8c
VladoDev03 <vladsto101@gmail.com>
2021-02-28 23:50:09 +0200

    Adding new methods in UserServices.

--- FancyLibrary/ConsoleVersion/Services/UserServices.cs
+++ FancyLibrary/ConsoleVersion/Services/UserServices.cs
@@ -20,9 +20,65 @@ namespace ConsoleVersion.Services
             return db.Users.ToList();
         }
 
+        public User FindUser(int id)
+        {
+            return db.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public User FindUser(string username)
+        {
+            return db.Users.FirstOrDefault(u => u.Username == username);
+        }
+
         public void AddUser(User user)
         {
             db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        public void AddBookToUser(User user, Book book)
+        {
+            db.UsersBooks.Add(new UserBook
+            {
+                UserId = user.Id,
+                BookId = book.Id
+            });
+
+            db.SaveChanges();
+        }
+
+        public void AddEmail(User user, string email)
+        {
+            if (user.Contact == null)
+            {
+                user.Contact = new Contact();
+            }
+
+            user.Contact.Email = email;
+
+            db.SaveChanges();
+        }
+
+        public void AddPhone(User user, string phone)
+        {
+            if (user.Contact == null)
+            {
+                user.Contact = new Contact();
+            }
+
+            user.Contact.Phone = phone;
+
+            db.SaveChanges();
+        }
+
+        public void ChangeUsername(User user, string username)
+        {
+            user.Username = username;
+        }
+
+        public void ChangePassword(User user, string password)
+        {
+            user.Password = password;
         }
     }
 }
