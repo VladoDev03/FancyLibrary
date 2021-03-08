3dd6ffcad999418c78636756c51244c5a8d81bf4
VladoDev03 <vladsto101@gmail.com>
2021-03-08 19:51:08 +0200

    Fixing bugs in AuthorServices class.

--- FancyLibrary/ConsoleVersion/Services/AuthorServices.cs
+++ FancyLibrary/ConsoleVersion/Services/AuthorServices.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleVersion.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleVersion.Services
@@ -12,6 +13,11 @@ namespace ConsoleVersion.Services
         public AuthorServices(FancyLibraryContext db)
         {
             this.db = db;
+        }
+
+        public List<Author> GetAllAuthors()
+        {
+            return db.Authors.ToList();
         }
 
         public void AddAuthor(Author author)
