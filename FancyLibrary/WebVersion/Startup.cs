00aac65749bdeeef9b8509bcba1f28ce556c6f37
VladoDev03 <vladsto101@gmail.com>
2021-03-09 20:55:56 +0200

    Register database.

--- FancyLibrary/WebVersion/Startup.cs
+++ FancyLibrary/WebVersion/Startup.cs
@@ -1,6 +1,8 @@
+using Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,11 @@ namespace WebVersion
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<FancyLibraryContext>(options =>
+            {
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseMySQL(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
