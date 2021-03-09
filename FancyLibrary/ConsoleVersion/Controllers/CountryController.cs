d829a5e416440e52e0858a7addc3842ba0314714
VladoDev03 <vladsto101@gmail.com>
2021-03-09 11:24:45 +0200

    Adding CountryService, interface for it and CountryController.

---
+++ FancyLibrary/ConsoleVersion/Controllers/CountryController.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleVersion.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleVersion.Controllers
+{
+    public class CountryController
+    {
+        private ICountryServices countryServices;
+
+        public CountryController(ICountryServices countryServices)
+        {
+            this.countryServices = countryServices;
+        }
+    }
+}
