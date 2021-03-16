06791f05be295539ee62b8dc66497a9f4091e5cc
VladoDev03 <vladsto101@gmail.com>
2021-03-17 01:39:52 +0200

    Fixing all views.

---
+++ FancyLibrary/WebVersion/wwwroot/js/Main.js
@@ -0,0 +1,16 @@
+﻿const text = "FancyLibrary"
+
+let index = 1;
+
+function writeText() {
+    let element = document.getElementById('title')
+
+    element.innerText = text.slice(0, index);
+    index++;
+
+    if (index > text.length) {
+        index = 1;
+    }
+}
+
+setInterval(writeText, 350);
